7129ba4d79ca46a5dbb8370d975cea5426a9f1d4
mrfragger <80211672+mrfragger@users.noreply.github.com>
2026-02-07 04:13:25 +0000

    Update substitcher.rb

--- Formula/substitcher.rb
+++ Formula/substitcher.rb
@@ -1,21 +1,18 @@
 class Substitcher < Formula
   desc "Audiobook encoder and player with subtitle support"
   homepage "https://github.com/mrfragger/substitcher"
-  version "26.01.24b"
+  version "VERSION_PLACEHOLDER"
   license "MIT"
-
   on_macos do
     on_arm do
-      url "https://github.com/mrfragger/substitcher/releases/download/v26.01.24b/SubStitcher-macOS-arm64.dmg"
-      sha256 "d35ced3de584d4965b1cf5f229268c3b58e3f639a93da6c3ea4e188e96cb9c5a"
+      url "https://github.com/mrfragger/substitcher/releases/download/vVERSION_PLACEHOLDER/SubStitcher-macOS-arm64.dmg"
+      sha256 "SHA_ARM64_PLACEHOLDER"
     end
   end
-
   def install
     prefix.install "SubStitcher.app"
     bin.write_exec_script "#{prefix}/SubStitcher.app/Contents/MacOS/SubStitcher"
   end
-
   test do
     assert_predicate prefix/"SubStitcher.app", :exist?
   end
